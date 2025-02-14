195d3a23e43cfb52fcd51a725c3f281ceff7a4c1
Alex Ro <alxroum@gmail.com>
2025-02-13 20:17:18 -0500

    progress on printing cards to terminal.

--- program.cpp
+++ program.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int cardWidth = 30;
+int cardHeight = 8;
 
 class Flashcard {
 
@@ -92,10 +94,62 @@ void drawCard() {
     return;
 }
 
-string splitLines(string s, int maxLineLength) { // takes a string of text and splits it into equal lines based on a max line length
+int displayCard(string *str, int maxLineLength) { // takes a string of text and splits it into equal lines based on a max line length
     // add "\n" to string in places where line breaks should occur
     // split string into words
     // create single string again from strings, but put line breaks to make lines
+    string &s = *str;
+    string lines[cardHeight] = {};
+    int line = 1;
+    int wordCount = maxLineLength;
+    int longestLine = 0; // longest line in character count
+    int prevWordIdx = 0;
+    for(int i = 0; i < s.size(); i++) {
+        //cout << s[i];
+        lines[line] += s[i];
+        if(s[i] == ' ') {
+            
+            wordCount--;
+            if(wordCount <= 0) {
+                //s[i] = '\n'; // uncomment this to put line breaks in the string
+                wordCount = maxLineLength;
+                if(i - prevWordIdx > maxLineLength) { // finding the length of the word
+                    longestLine = i - prevWordIdx;
+                }
+                prevWordIdx = i; // location that the previous word starts
+                line++;
+            }
+        }
+    }
+    string spaces = "|";
+    string dashes = "";
+    for(int i = 0; i < cardWidth - 2; i++) {
+        spaces += " ";
+        dashes += "-";
+    }
+    spaces += "|";
+    lines[0] = "." + dashes;
+    lines[cardHeight - 1] = "'" + dashes + "'\n";
+
+    // add spacing to center text
+    for(int i = 0; i < cardWidth; i++) { // not exiting this loop
+        
+        int padding = (cardWidth - lines[i].size()) / 2;
+        string pad = "";
+        for(int j = 0; j < padding; j++) {
+            pad += " ";
+        }
+        lines[i] = pad + lines[i];
+    }
+
+    // print out lines
+    for(int i = 0; i < cardHeight; i++) {
+        cout << lines[i] + "\n";
+    }
+
+
+    str = &s; // reassigning the new string to the original pointer
+    return longestLine;
     
 }
 
@@ -121,8 +175,9 @@ int main(int argc, const char* argv[]) {
             // practice set mode;
             cout << "View Mode:" << endl;
             _vwMode = 1;
-            drawCard();
-            cout << splitLines("This is a test card in which I will test the splitlines function", 15) << endl;
+            string inputStr = "This is a test card in which I will test the splitlines function";
+            cout << displayCard(&inputStr, 2) << endl;
+            //cout << inputStr << endl;
         } 
     } else {
         cout << "View Mode:" << endl;
